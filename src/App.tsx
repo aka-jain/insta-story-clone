0ab520a79b739d589666200474233f34bd87d040
aka-jain <akashjain993@gmail.com>
2025-04-15 01:12:52 +0530

    Add project skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.scss'
-import StageComponent from './components/StageComponent'
 
 function App() {
   return (
     <div className='flex flex-col gap-2'>
       <h1 className='text-center text-xl mt-1 font-medium text-gray-700'>Stage</h1>
-      <StageComponent />
     </div>
   )
 }
